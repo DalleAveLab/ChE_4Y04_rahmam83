aa377e18921270d2a686d1035eaaa6676a0d1ddf
Mashroor Rahman <your-email@example.com>
2026-03-07 05:06:15 -0500

    Updating to inlcude addition of new models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,13 +1,21 @@
-"""KAN model variants for TEP fault detection."""
+"""Model registry for TEP fault detection."""
 
 from .efficient_kan import EfficientKAN
 from .fourier_kan import FourierKAN
 from .wavelet_kan import WaveletKAN
 from .fast_kan import FastKAN
+from .mlp import MLP
+from .cnn import CNN
+from .rnn import RNN
+from .lstm import LSTM
 
 MODEL_REGISTRY = {
     'efficient_kan': EfficientKAN,
     'fourier_kan':   FourierKAN,
     'wavelet_kan':   WaveletKAN,
     'fast_kan':      FastKAN,
+    'mlp':           MLP,
+    'cnn':           CNN,
+    'rnn':           RNN,
+    'lstm':          LSTM,
 }
